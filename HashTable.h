991294b8bdbcc3aa0a3bce5ae550b6efe03a4ea4
hectorigual <igualhector@gmail.com>
2023-12-26 14:33:24 +0100

    añadido

--- HashTable.h
+++ HashTable.h
@@ -32,22 +32,70 @@ class HashTable: public Dict<V> {
 		table = new ListLinked<TableEntry<V>>[max];
 	}
 	~HashTable(){
+	//	for(int i= 0; i< max; i++){
+	//		delete table[i];
+	//	}
 		delete[] table;
 	}
 	int capacity(){
 		return max;
 	}
 	friend std::ostream& operator<<(std::ostream &out, const HashTable<V>&th){
-		out<<th.n<<" "<<th.table<<std::endl;
+		out << "HashTable [Entries: " << th.n << ", Capacity: " << th.max << "]\n";
+		for(int i = 0; i < th.max; i++){
+
+			out << "Cubeta : " << i << " \n";
+	
+			out<<th.table[i]<<std::endl;
+		}
 	      return out;	
 	}
 	V operator[](std::string key){
-		V valor= table[h(key)].search(key);
-		if(valor != ""){
+		int pos = table[h(key)].search(key);
+
+		if(pos < 0)
+			throw std::runtime_error("Error en la búsqueda");
+		
+		V valor = table[h(key)].get(pos).value;
 		return valor;
-		}else{
-			throw std::runtime_error("error");
-		}
+	}
+
+	 void insert(std::string key, V value) override{	
+
+		int pos = table[h(key)].search(key);
+
+                if(pos >= 0)
+                        throw std::runtime_error("Error al introducir");
+
+		TableEntry<V> aux(key, value);
+		table[h(key)].prepend(aux);
+		n++;
+	}
+	 V search(std::string key) override{
+
+		int hash = h(key);
+		int pos = table[h(key)].search(key);
+
+                if(pos < 0)
+                        throw std::runtime_error("No encontrado");
+
+                V aux = table[hash].get(pos).value;
+                return aux;
+	}
+        V remove(std::string key) override{
+		
+		int pos = table[h(key)].search(key);
+
+                if(pos > max || pos < 0)
+                        throw std::runtime_error("No encontrado");
+
+		V aux = table[h(key)].remove(pos).value;
+		n--;
+		return aux;
+	}
+
+        int entries() override{
+		return n;
 	}
         
 };
